2060ba0627351f896562a44793929b4987f74f44
dostse <49457298+dostse@users.noreply.github.com>
2021-10-30 23:56:40 +0200

    Added Params to run file

--- Run_Test.ps1
+++ Run_Test.ps1
@@ -1,15 +1,20 @@
-$APIToken = (Import-Clixml .\APIKey.xml).GetNetworkCredential().Password
-$BudgetName = "Test"
-$AccountName = "TestAcc"
+param([Parameter(Mandatory = $false)]
+[string]
+$APIToken = (Import-Clixml .\APIKey.xml).GetNetworkCredential().Password,
+[Parameter(Mandatory = $false)]
+[string]
+$BudgetName = "Test",
+[Parameter(Mandatory = $false)]
+[string]
+$AccountName = "TestAcc",
+[Parameter(Mandatory = $true)]
+[string]
+$CSVFile,
+[Parameter(Mandatory = $false)]
+[string]
+$CSVOut = "c:\Temp\YNAB_Test_$(Get-Date -Format yyyyMMdd_HHmmss).csv"
+)
 
-$CSVIn = "C:\Temp\Transaktioner_2021-10-29_09-24-16.csv"
-#$KomplettCSV = "C:\Temp\transaktioner (1).csv"
-
-$CSVOut = "c:\Temp\YNAB_SwedBank_$(Get-Date -Format yyyyMMdd_HHmmss).csv"
-#$KomplettCSVOut = "c:\Temp\YNAB_KOMPLETT_$(Get-Date -Format yyyyMMdd_HHmmss).csv"
-
-.\Convert-YNABSwedbankCSV.ps1 -InPath $CSVIn -OutPath $CSVOut
-#.\Convert-YNABKomplettCSV.ps1 -InPath $KomplettCSV -OutPath $KomplettCSVOut
+.\Convert-YNABSwedbankCSV.ps1 -InPath $CSVFile -OutPath $CSVOut
 
 .\Import-YNABCSVFile.ps1 -APIToken $APIToken -BudgetName $BudgetName -AccountName $AccountName  -CSVFile $CSVOut
-#.\Import-YNABCSVFile.ps1 -APIToken $APIToken -BudgetName $BudgetName -AccountName "" -CSVFile $KomplettCSVOut
