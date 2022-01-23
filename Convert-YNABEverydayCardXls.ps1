fee0d3014c30eb6173668989a6e0fae36e026b76
dostse <49457298+dostse@users.noreply.github.com>
2022-01-23 09:57:43 +0100

    Added code to handle positive transactions

--- Convert-YNABEverydayCardXls.ps1
+++ Convert-YNABEverydayCardXls.ps1
@@ -6,7 +6,6 @@ $InPath,
 $OutPath
 )
 
-
 ConvertTo-ExcelXlsx -Path $InPath 
 $XLSX = Import-Excel -Path "$($InPath)x" -StartRow 6
 $Headers = ($XLSX | Get-Member -MemberType NoteProperty).Name
@@ -16,11 +15,19 @@ $Table = @()
 if($Headers -contains "Datum" -and $Headers -contains "Specifikation" -and $Headers -contains "Belopp SEK" ){
 
     foreach($Item in $XLSX){
+        if ([int]$Item."Belopp SEK" -ge 0){
+
+            $Amount = -($Item."Belopp SEK")
+
+        }
+        elseif ([int]$Item."Belopp SEK" -lt 0) {
+            $Amount = [Math]::Abs($Item."Belopp SEK")
+        }
 
         $Properties = [ordered]@{       'Date' = $Item.Datum 
                                         'Payee' = $Item.Specifikation
                                         'Memo' = ""
-                                        'Amount' = "-$($Item."Belopp SEK")"
+                                        'Amount' = $Amount
                 }
     
         $obj = New-Object -TypeName psobject -Property $Properties
